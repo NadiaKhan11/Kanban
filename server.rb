bbe513fd4e89b55f14a6bab28969ef4504254c7d
NadiaKhan11 <nadia.khan@sky.uk>
2019-08-30 12:00:00 +0100

    Added a columns GET route

--- server.rb
+++ server.rb
@@ -1,8 +1,17 @@
 require "sinatra"
 require "sinatra/activerecord"
+require "json"
+
+require_relative './app/models/ticket.rb'
 
 class Server < Sinatra::Base
     get '/' do
         erb :index
+    end
+
+    get '/columns' do
+        columns = Ticket.all.group_by { |ticket| ticket.column }
+        columns = columns.map { |col| [col.first.column, col] }.to_h
+        column.to_json
     end
 end
